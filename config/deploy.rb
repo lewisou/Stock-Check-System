0f41b50ea2981963eee91dc4f55ccff6a01a0a7f
Lewisou <lewisou@gmail.com>
2011-04-22 15:07:26 +0800

    add capify

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+set :application, "scs"
+set :repository,  "git@notforget.net:scs"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "192.168.1.199"                          # Your HTTP server, Apache/etc
+role :app, "192.168.1.199"                          # This may be the same as your `Web` server
+role :db,  "192.168.1.199", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
